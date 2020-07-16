1988dae5b431990f41c46e741623a0a6b6b5a34b
Isai86 <isaitd19@hotmail.com>
2020-07-16 16:53:58 -0500

    Updates

---
+++ precache-manifest.4b33a44b703a528889398f9f79524e33.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0c2849dd934731852cae06a9552fdd",
+    "url": "/MenuQrwebsite/index.html"
+  },
+  {
+    "revision": "822699ad6cbb3516e2ed",
+    "url": "/MenuQrwebsite/static/css/main.47cf5660.chunk.css"
+  },
+  {
+    "revision": "2c1e64ac732eea3851d7",
+    "url": "/MenuQrwebsite/static/js/2.9b2a552c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MenuQrwebsite/static/js/2.9b2a552c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "822699ad6cbb3516e2ed",
+    "url": "/MenuQrwebsite/static/js/main.ab220550.chunk.js"
+  },
+  {
+    "revision": "0835ec1843e60e79075f",
+    "url": "/MenuQrwebsite/static/js/runtime-main.2d13dae5.js"
+  },
+  {
+    "revision": "435521e8f08a580e834d5462e261dfae",
+    "url": "/MenuQrwebsite/static/media/chef.435521e8.svg"
+  },
+  {
+    "revision": "041891e0b0f864673c8aa44bd4e954d4",
+    "url": "/MenuQrwebsite/static/media/clock.041891e0.svg"
+  },
+  {
+    "revision": "e83db8f1bbb7dac5674c358e105e4c63",
+    "url": "/MenuQrwebsite/static/media/fondoBlur.e83db8f1.jpg"
+  },
+  {
+    "revision": "792612b1eb4279b113434c471ca444b3",
+    "url": "/MenuQrwebsite/static/media/hand.792612b1.svg"
+  },
+  {
+    "revision": "6150ad2fb34a7f0c300f4d97321b88fe",
+    "url": "/MenuQrwebsite/static/media/improvement.6150ad2f.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MenuQrwebsite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "beb09550d321a94ee4dc9d60c91d38f3",
+    "url": "/MenuQrwebsite/static/media/mdigital.beb09550.svg"
+  },
+  {
+    "revision": "5be51a7439c899fd913ebe8190a812d9",
+    "url": "/MenuQrwebsite/static/media/menus1.5be51a74.png"
+  },
+  {
+    "revision": "7260d761dbdbef6c406a945262a38edb",
+    "url": "/MenuQrwebsite/static/media/pizza.7260d761.jpg"
+  },
+  {
+    "revision": "be2e38ac02a1ed4abaac12001bfacf85",
+    "url": "/MenuQrwebsite/static/media/plant.be2e38ac.svg"
+  },
+  {
+    "revision": "214a7b232f928b86272599cfb9183b00",
+    "url": "/MenuQrwebsite/static/media/precaution.214a7b23.svg"
+  },
+  {
+    "revision": "89b2d1843f5bf33d32499b0103e48c15",
+    "url": "/MenuQrwebsite/static/media/qr.89b2d184.svg"
+  },
+  {
+    "revision": "5e098af911d6ef1896955aebe12feb1d",
+    "url": "/MenuQrwebsite/static/media/star.5e098af9.svg"
+  }
+]);
